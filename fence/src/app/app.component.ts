17eb55bd9fd93ef206536991173886116e268079
johncolecarter <colecarter7777@gmail.com>
2020-04-14 10:19:38 -0500

    sorting and filtering

--- fence/src/app/app.component.ts
+++ fence/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { IFence } from '../app/interfaces/IFence';
 import { FenceServiceService } from './services/fence-service.service';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,20 @@ export class AppComponent implements OnInit {
   displayedColumns: string[] = ['id', 'name', 'streetAddress', 'city'];
   dataSource: MatTableDataSource<IFence>;
 
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   constructor(private fenceServiceService: FenceServiceService) {
 
   }
 
   ngOnInit() {
     this.dataSource = new MatTableDataSource(this.fenceServiceService.getFence());
+
+    this.dataSource.sort = this.sort;
   }
 }
